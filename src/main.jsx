d9f9d8e1d4d7eb27e302fc92a401ca62a6510224
Mekkemii <99206884+Mekkemii@users.noreply.github.com>
2025-04-15 11:44:32 +0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Daily100Tracker from './Daily100Tracker'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Daily100Tracker />
+  </React.StrictMode>,
+)
